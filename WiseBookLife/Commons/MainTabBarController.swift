c9315e0b9358bfffecfa1b2ab4329df1f79c5ba3
bokyungseo <maybutter756@gmail.com>
2021-08-17 23:23:07 +0900

    TabBar 배경색 변경

---
+++ WiseBookLife/Commons/MainTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabBarController.swift
+//  WiseBookLife
+//
+//  Created by 서보경 on 2021/08/16.
+//  Copyright © 2021 서보경. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTabBarBackground()
+    }
+    
+    func setTabBarBackground() {
+        self.tabBar.barTintColor = .systemBackground
+//        self.tabBar.isTranslucent = false
+    }
+
+}
