27fe9f286debec920bd1b395045190ba2f968e22
bokyungseo <maybutter756@gmail.com>
2021-10-29 19:46:33 +0900

    Database: 기록 데이터 구조 변경

--- WiseBookLife/Commons/Common.swift
+++ WiseBookLife/Commons/Common.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 struct BookItem: Codable, Hashable {
     var title: String = ""
@@ -29,10 +30,33 @@ struct BookItem: Codable, Hashable {
     }
 }
 
-struct Record: Codable {
-    var bookData: BookItem = BookItem()
-    var contents: [Date: String] = [:]
+class BookInfo: Object, Codable {
+    @objc dynamic var title: String = ""
+    @objc dynamic var link: String = ""
+    @objc dynamic var image: String = ""
+    @objc dynamic var author: String = ""
+    @objc dynamic var price: String = ""
+//    @objc dynamic var discount: String = ""
+    @objc dynamic var publisher: String = ""
+    @objc dynamic var isbn: String = ""
+    @objc dynamic var descriptionBook: String = ""
+    @objc dynamic var pubdate: String = ""
+    
+    func modifySearchedData() {
+        let title = self.title.removeHTMLTag()
+        let author = self.author.removeHTMLTag()
+        
+        self.title = title
+        self.author = author
+    }
 }
+
+class RecordContent: Object {
+    @objc dynamic var isbn: String = ""
+    @objc dynamic var date: Date = Date()
+    @objc dynamic var text: String = ""
+}
+
 
 extension Date {
     func dateToString() -> String {
@@ -40,60 +64,5 @@ extension Date {
         df.dateFormat = "yyyy-MM-dd"
         
         return df.string(from: self)
-    }
-}
-
-// MARK:- Heart Dictionary / Search Condition Model Array
-
-struct CommonData {
-    static var shared = CommonData()
-    
-    var heartDic: [String: BookItem] = [:] // ISBN: Item
-    var records: [String: Record] = [:] // ISBN: Record
-    // 최신순으로 정렬하기 위한 기준이 필요함
-    var latest: [String] = []
-    
-    
-    func sortRecords(isbn: String) -> [(Date, String)] {
-        guard let data = records[isbn] else { return [] }
-        
-        return data.contents.sorted { $0.key < $1.key }
-    }
-    
-    func createCalendarModel() -> [String: [BookItem]] { // date_string: [Item]
-        
-        var calendarModel: [String: [BookItem]] = [:]
-        let values = records.values
-        
-        for item in values {
-            let bookitem = item.bookData
-            for data in item.contents {
-                let df = DateFormatter()
-                df.dateFormat = "yyyy-MM-dd"
-                let calendarKey = df.string(from: data.key)
-                
-                var newItemSets: Set<BookItem> = []
-                
-                if let prevItem = calendarModel[calendarKey] {
-                    newItemSets = Set(prevItem)
-                }
-                newItemSets.insert(bookitem)
-                let newItem = newItemSets.sorted { $0.title < $1.title }
-                // 기록 업데이트 순으로 정렬할 방법?
-                
-                calendarModel.updateValue(newItem, forKey: df.string(from: data.key))
-                
-            }
-        }
-        
-        return calendarModel
-    }
-    
-    func createMyLibraryList() -> [BookItem] {
-        var librarylist = records.map { $0.value.bookData }
-        librarylist.sort { $0.title < $1.title }
-        // records 데이터 변경 최신 순으로 정렬이 필요함
-        // 현재는 이름 순 정렬 중
-        return librarylist
     }
 }
