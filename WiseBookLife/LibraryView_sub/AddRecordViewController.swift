98f8e17d85eb70d7b4242244a0f2181e5481cf5d
bokyungseo <maybutter756@gmail.com>
2021-11-06 19:59:49 +0900

    Fixed #9

--- WiseBookLife/LibraryView_sub/AddRecordViewController.swift
+++ WiseBookLife/LibraryView_sub/AddRecordViewController.swift
@@ -7,18 +7,37 @@
 //
 
 import UIKit
+import NotificationCenter
 
 class AddRecordViewController: UIViewController {
 
     var newRecordContent = RecordContent()
     var placeholderText = "기록을 작성해보세요!"
     
+    @IBOutlet weak var textViewBottomConstraint: NSLayoutConstraint!
+    
     @IBOutlet weak var recordInputView: UITextView!
     @IBOutlet weak var recordDatePicker: UIDatePicker!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         recordInputView.delegate = self
         settingTextView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
     }
     
     func settingTextView() {
@@ -48,11 +67,22 @@ class AddRecordViewController: UIViewController {
     }
 }
 
+extension AddRecordViewController {
+    @objc func keyboardWillShow(_ noti: NSNotification) {
+        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            textViewBottomConstraint.constant = 20 + keyboardHeight
+        }
+    }
+    @objc func keyboardWillHide(_ noti: NSNotification) {
+        textViewBottomConstraint.constant = 20
+    }
+}
+
 extension AddRecordViewController: UITextViewDelegate {
     func textViewDidBeginEditing(_ textView: UITextView) {
         if textView.text == self.placeholderText {
             textView.text = ""
-            
         }
     }
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
