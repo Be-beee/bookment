46a2e521c8938b8d069896e93f059461f0990c76
bokyungseo <maybutter756@gmail.com>
2022-12-27 18:25:46 +0900

    refactor: MainPageViewController 리팩토링

--- WiseBookLife/Presentations/MainPageViewController.swift
+++ WiseBookLife/Presentations/MainPageViewController.swift
@@ -7,42 +7,59 @@
 //
 
 import UIKit
+
 import RealmSwift
 
-class MainPageViewController: UIViewController {
+final class MainPageViewController: UIViewController {
+    
+    // MARK: - UI Properties
+    
     @IBOutlet weak var recordContainerView: UIView!
     @IBOutlet weak var wishListContainerView: UIView!
     @IBOutlet weak var recordViewMenu: UILabel!
     @IBOutlet weak var wishListViewMenu: UILabel!
     @IBOutlet weak var floatingPlusBtn: UIButton!
     
-    var selectedIndex = 0
+    // MARK: - Properties
+    
+    private var selectedPage = MainPageType.record {
+        didSet {
+            configurePageView()
+        }
+    }
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        removeNavigationBarLine()
-        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: nil, style: .plain, target: nil, action: nil)
-        settingInitialView()
-        settingMenuGesture()
-        print(Realm.Configuration.defaultConfiguration.fileURL!) // realm file path
+        
+        configurePageView()
+        configureFloatingButton()
+        configureMenuGesture()
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL) // realm file path
     }
     
-    func settingInitialView() {
-        recordContainerView.isHidden = false
-        wishListContainerView.isHidden = true
-        
-        recordViewMenu.font = UIFont.boldSystemFont(ofSize: 17)
-        recordViewMenu.textColor = Theme.main.mainColor
-        wishListViewMenu.textColor = .label
-        
+    // MARK: - Configure Functions
+    
+    /// 처음 메인화면이 나타날 때 보여질 화면을 설정한다. 앱 최초 진입시에는 나의 노트가 먼저 보인다.
+    private func configurePageView() {
+        let isRecordViewHidden = (selectedPage == MainPageType.record) ? false : true
+        recordContainerView.isHidden = isRecordViewHidden
+        wishListContainerView.isHidden = !recordContainerView.isHidden
+    }
+    
+    /// 도서 검색 플로팅 버튼 UI 설정
+    private func configureFloatingButton() {
         floatingPlusBtn.layer.cornerRadius = floatingPlusBtn.bounds.size.width/2
         
         floatingPlusBtn.layer.shadowColor = UIColor.systemGray.cgColor
-        floatingPlusBtn.layer.shadowOpacity = 0.5
-        floatingPlusBtn.layer.shadowOffset = CGSize(width: 2, height: 2)
+        floatingPlusBtn.layer.shadowOpacity = Metric.floatingButtonShadowOpacity
+        floatingPlusBtn.layer.shadowOffset = Metric.floatingButtonShadowOffset
     }
     
-    func settingMenuGesture() {
+    /// 메뉴 버튼의 GestureRecognizer 설정
+    private func configureMenuGesture() {
         let recordViewGesture = UITapGestureRecognizer(target: self, action: #selector(showRecordView))
         let wishListViewGesture = UITapGestureRecognizer(target: self, action: #selector(showWishListView))
         
@@ -50,28 +67,46 @@ class MainPageViewController: UIViewController {
         wishListViewMenu.addGestureRecognizer(wishListViewGesture)
     }
     
+    // MARK: - objc Functions
+    
     @objc func showRecordView() {
-        recordContainerView.isHidden = false
-        wishListContainerView.isHidden = true
+        selectedPage = MainPageType.record
         
-        recordViewMenu.font = UIFont.boldSystemFont(ofSize: 17)
-        recordViewMenu.textColor = Theme.main.mainColor
-        wishListViewMenu.font = UIFont.systemFont(ofSize: 17)
-        wishListViewMenu.textColor = .label
-    }
-    @objc func showWishListView() {
-        recordContainerView.isHidden = true
-        wishListContainerView.isHidden = false
-        
-        recordViewMenu.font = UIFont.systemFont(ofSize: 17)
-        recordViewMenu.textColor = .label
-        wishListViewMenu.font = UIFont.boldSystemFont(ofSize: 17)
-        wishListViewMenu.textColor = Theme.main.mainColor
+        recordViewMenu.textColor = .primary
+        wishListViewMenu.textColor = .systemGray
     }
     
+    @objc func showWishListView() {
+        selectedPage = MainPageType.wishlist
+        
+        recordViewMenu.textColor = .systemGray
+        wishListViewMenu.textColor = .primary
+    }
+    
+    // MARK: - IBAction Functions
     
     @IBAction func moveToAddContentsView(_ sender: UIButton) {
-        guard let searchVC = UIStoryboard(name: "MainSearchResultViewController", bundle: nil).instantiateViewController(withIdentifier: "MainSearchResultViewController") as? MainSearchResultViewController else { return }
+        let mainSearchResultViewID = MainSearchResultViewController.name
+        guard let searchVC = loadViewController(with: mainSearchResultViewID) as? MainSearchResultViewController
+        else { return }
+        
         self.navigationController?.pushViewController(searchVC, animated: true)
+    }
+}
+
+// MARK: - Pages
+
+extension MainPageViewController {
+    enum MainPageType: Int {
+        case record, wishlist
+    }
+}
+
+// MARK: - Namespaces
+
+extension MainPageViewController {
+    enum Metric {
+        static let floatingButtonShadowOpacity: Float = 0.5
+        static let floatingButtonShadowOffset = CGSize(width: 2, height: 2)
     }
 }
