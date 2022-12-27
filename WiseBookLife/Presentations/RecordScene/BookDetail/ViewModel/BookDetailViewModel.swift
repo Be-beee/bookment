56abd8d61d73705811c023ff8dc510a934347f60
bokyungseo <maybutter756@gmail.com>
2022-12-27 19:55:16 +0900

    refactor: View와 ViewModel 폴더 생성 및 파일 이동

---
+++ WiseBookLife/Presentations/RecordScene/BookDetail/ViewModel/BookDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BookDetailViewModel.swift
+//  WiseBookLife
+//
+//  Created by 서보경 on 2022/12/27.
+//  Copyright © 2022 서보경. All rights reserved.
+//
+
+import Foundation
+
+final class BookDetailViewModel {
+    
+}
