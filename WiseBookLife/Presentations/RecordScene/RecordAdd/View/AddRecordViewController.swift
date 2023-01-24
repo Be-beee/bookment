ed2223f88c948c9b0e1e29d6a739dad3a666f4d6
bokyungseo <maybutter756@gmail.com>
2023-01-24 18:11:01 +0900

    refactor: AddRecordViewController네임스페이스 정리

--- WiseBookLife/Presentations/RecordScene/RecordAdd/View/AddRecordViewController.swift
+++ WiseBookLife/Presentations/RecordScene/RecordAdd/View/AddRecordViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
-import NotificationCenter
 
-class AddRecordViewController: UIViewController {
+final class AddRecordViewController: UIViewController {
 
-    var newRecordContent = RecordContent()
-    var placeholderText = "기록을 작성해보세요!"
+    // MARK: - UI Properties
     
     @IBOutlet weak var textViewBottomConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var recordInputView: UITextView!
     @IBOutlet weak var recordDatePicker: UIDatePicker!
+    
+    // MARK: - Properties
+    
+    var viewModel: AddRecordViewModel?
+    
+    var newRecordContent = RecordContent()
+    
+    // MARK: - Life Cycle
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         self.view.endEditing(true)
@@ -25,71 +31,109 @@ class AddRecordViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        recordInputView.delegate = self
-        settingTextView()
+        
+        configureTextView()
+        configureTextViewDelegate()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        addKeyboardObservers()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        
+        removeKeyboardObservers()
+    }
+    
+    // MARK: - Configure Functions
+    
+    private func configureTextView() {
+        recordInputView.layer.borderWidth = Metric.textViewBorderWidth
+        recordInputView.layer.borderColor = UIColor.primary?.cgColor
+        recordInputView.backgroundColor = .systemBackground
+        recordInputView.text = StringLiteral.placeholderText
+        recordInputView.textColor = .systemGray
+    }
+    
+    private func configureTextViewDelegate() {
+        recordInputView.delegate = self
+    }
+    
+    // MARK: - Notification Center Functions
+    
+    private func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func removeKeyboardObservers() {
         NotificationCenter.default.removeObserver(self)
     }
     
-    func settingTextView() {
-        recordInputView.layer.borderWidth = 0.3
-        recordInputView.layer.borderColor = UIColor.primary?.cgColor
-        recordInputView.backgroundColor = .systemBackground
-        recordInputView.text = self.placeholderText
-        recordInputView.textColor = .systemGray
-    }
+    // MARK: - IBAction Functions
     
     @IBAction func dismissThisView(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func saveRecord(_ sender: UIButton) {
+        // TODO: ViewModel로 옮기고 presentAlert 부분은 delegate로 전달
         self.newRecordContent.date = recordDatePicker.date
         self.newRecordContent.text = recordInputView.text
         if !newRecordContent.text.isEmpty, recordInputView.textColor == .label {
             self.performSegue(withIdentifier: "toDetailViewFromAddView", sender: self)
         } else {
-            let alert = UIAlertController(title: "알림", message: "내용을 입력해주세요!", preferredStyle: .alert)
-            let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
-            alert.addAction(ok)
-            
-            self.present(alert, animated: true, completion: nil)
+            presentNoticeAlert(with: StringLiteral.noticeMessage)
         }
     }
 }
+
+// MARK: - Keyboard
 
 extension AddRecordViewController {
     @objc func keyboardWillShow(_ noti: NSNotification) {
         if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
             let keyboardHeight = keyboardFrame.cgRectValue.height
-            textViewBottomConstraint.constant = 20 + keyboardHeight
+            textViewBottomConstraint.constant = Metric.bottomSpacing + keyboardHeight
         }
     }
     @objc func keyboardWillHide(_ noti: NSNotification) {
-        textViewBottomConstraint.constant = 20
+        textViewBottomConstraint.constant = Metric.bottomSpacing
     }
 }
 
+// MARK: - UITextViewDelegate
+
 extension AddRecordViewController: UITextViewDelegate {
     func textViewDidBeginEditing(_ textView: UITextView) {
-        if textView.text == self.placeholderText {
-            textView.text = ""
+        if textView.text == StringLiteral.placeholderText {
+            textView.text = StringLiteral.emptyText
         }
     }
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         textView.textColor = .label
-        if textView.text == self.placeholderText {
-            textView.text = ""
+        if textView.text == StringLiteral.placeholderText {
+            textView.text = StringLiteral.emptyText
         }
         return true
+    }
+}
+
+// MARK: - Namespaces
+
+extension AddRecordViewController {
+    enum Metric {
+        static let textViewBorderWidth: CGFloat = 0.3
+        static let bottomSpacing: CGFloat = 20
+    }
+    
+    enum StringLiteral {
+        static let placeholderText = "기록을 작성해보세요!"
+        static let emptyText = ""
+        
+        static let noticeMessage = "내용을 입력해주세요!"
     }
 }
---
+++ WiseBookLife/Presentations/RecordScene/RecordAdd/ViewModel/AddRecordViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddRecordViewModel.swift
+//  WiseBookLife
+//
+//  Created by 서보경 on 2023/01/24.
+//  Copyright © 2023 서보경. All rights reserved.
+//
+
+import Foundation
+
+final class AddRecordViewModel {
+    
+}
